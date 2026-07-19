c9362b45053e5814b5777cd8bf8e03b93261545b
MFR Rayhan <md.fazlerabbirayhan786@gmail.com>
2026-07-19 07:28:44 +0600

    Fetch and display random foods from API in Foods component

--- src/app/foods/page.jsx
+++ src/app/foods/page.jsx
@@ -1,3 +1,16 @@
-export default function Foods() {
-  return <div>Foods</div>;
+const getFoods = async () => {
+  const res = await fetch(
+    "https://taxi-kitchen-api.vercel.app/api/v1/foods/random",
+  );
+
+  const data = await res.json();
+
+  return data.foods || [];
+};
+
+export default async function Foods() {
+  const foods = await getFoods();
+  console.log(foods);
+
+  return <div>Total {foods.length} Foods Found</div>;
 }
